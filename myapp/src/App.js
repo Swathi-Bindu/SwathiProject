047600f03f88c1ff4782e16321c0545a4eee658e
Swathi <swathibindu.j@gmail.com>
2024-03-13 21:36:34 +0530

    Home and login page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react"
+import LoginPage from "./login";
+import HomePage from "./home"
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState(null)
+  const handleLogin = (username) => {
+    setLoggedInUser(username)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loggedInUser ? (
+        <HomePage/>
+      ) : (
+        <LoginPage onLogin={handleLogin} />
+      )}
     </div>
   );
 }
